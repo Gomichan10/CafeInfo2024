07997256a47ba258a8ce3890bd90afbdfbaeae35
Gomichan10 <cckg1220113@gn.iwasaki.ac.jp>
2024-01-28 10:26:50 +0900

    追跡開始と追跡解除を視覚的に分かりやすくした

--- CafeInfo2024/ViewController.swift
+++ CafeInfo2024/ViewController.swift
@@ -9,16 +9,20 @@ import UIKit
 import GoogleMaps
 import GooglePlaces
 
-class ViewController: UIViewController, CLLocationManagerDelegate {
+class ViewController: UIViewController, CLLocationManagerDelegate,GMSMapViewDelegate{
     
     var mapView: GMSMapView!
     var locationManager = CLLocationManager()
+    var isTrackingUserLocation: Bool = true
+    var tapCheck: Bool = true
+    let centerButton = UIButton(frame: CGRect(x: 300, y: 600, width: 60, height: 60))
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        setupMap()
         requestLocation()
+        setupMap()
+        centerMapOnUserButtonClicked()
         
     }
 
@@ -27,21 +31,36 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
         let camera = GMSCameraPosition.camera(withLatitude: 35.6812226, longitude: 139.7670594, zoom: 12.0)
         mapView = GMSMapView.map(withFrame: CGRect.zero, camera: camera)
         mapView.isMyLocationEnabled = true
+        mapView.delegate = self
         view = mapView
         
-        let centerButton = UIButton(frame: CGRect(x: 300, y: 600, width: 75, height: 75))
         centerButton.backgroundColor = .white
         centerButton.tintColor = .black
         centerButton.setImage(UIImage(systemName: "figure.stand"), for: .normal)
         centerButton.addTarget(self, action: #selector(centerMapOnUserButtonClicked), for: .touchUpInside)
-        centerButton.layer.cornerRadius = 37.0
+        centerButton.layer.cornerRadius = 30.0
         self.view.addSubview(centerButton)
     }
     
     @objc func centerMapOnUserButtonClicked() {
+        if tapCheck == true {
         if let location = locationManager.location {
             mapView.animate(toLocation: location.coordinate)
+            tapCheck = false
+            centerButton.tintColor = .white
+            centerButton.backgroundColor = .black
+            print(tapCheck)
+            return
+            }
         }
+        else if tapCheck == false{
+            tapCheck = true
+            centerButton.tintColor = .black
+            centerButton.backgroundColor = .white
+            print(tapCheck)
+            return
+        }
+        print(isTrackingUserLocation)
     }
 
     func requestLocation(){
@@ -51,9 +70,12 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
     }
     
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-        /*if let location = locations.first {
-            mapView.camera = GMSCameraPosition.camera(withLatitude: location.coordinate.latitude, longitude: location.coordinate.longitude, zoom: 15.0)
-        }*/
+        if tapCheck == false {
+            if let location = locations.first{
+                let updatedLocation = CLLocationCoordinate2D(latitude: location.coordinate.latitude, longitude: location.coordinate.longitude)
+                mapView.animate(toLocation: updatedLocation)
+            }
+        }
     }
     
     func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
@@ -61,7 +83,7 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
             locationManager.startUpdatingLocation()
         }
     }
-
+    
 }
 
 
