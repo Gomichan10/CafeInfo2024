5858463491ecb350a07f50a2a122832da1605690
Gomichan10 <cckg1220113@gn.iwasaki.ac.jp>
2024-01-29 15:14:10 +0900

    星評価機能を実装

---
+++ CafeInfo2024/AddSpotViewController.swift
@@ -0,0 +1,29 @@
+//
+//  AddSpotViewController.swift
+//  CafeInfo2024
+//
+//  Created by Gomi Kouki on 2024/01/29.
+//
+
+import UIKit
+
+class AddSpotViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
